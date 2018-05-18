5630a58065ac3d4cce985b8da987bf9e6ece5558
rpm@malloys.us <rpm@malloys.us>
2018-05-17 18:28:28 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,34 @@
-POLICY_SERVER=cfengine-policy.supported.systems
-CFENGINE_CHECKOUT_DIR=/root/cfengine
-CFENGINE_REPO_URL=https://github.com/rsp2k/freebsd-cfengine
-CFENGINE_WORK_DIR=/var/cfengine
-CFSERVERD_BIND_IP=0.0.0.0
-CFENGINE_NETWORK=10.1.16.0/20
-CFENGINE_REPLACE_TOKENS="POLICY_SERVER CFENGINE_WORK_DIR CFSERVERD_BIND_IP CFENGINE_NETWORK"
-E
-SMTP_SERVER=0.0.0.0
-MAIL_FROM=cfengine@example.com
-MAIL_TO=ryan@supported.systems
+#!/bin/sh
+
+POLICY_SERVER=${POLICY_SERVER:-cfengine-policy.supported.systems}
+CFENGINE_CHECKOUT_DIR=${CFENGINE_CHECKOUT_DIR:-/root/cfengine}
+CFENGINE_REPO_URL=${CFENGINE_REPO_URL:-https://github.com/rsp2k/freebsd-cfengine/trunk/cfengine}
+CFENGINE_WORK_DIR=${CFENGINE_WORK_DIR:-/var/cfengine}
+CFSERVERD_BIND_IP=${CFSERVERD_BIND_IP:-0.0.0.0}
+CFENGINE_NETWORK=${CFENGINE_NETWORK:-10.1.16.0/20}
+SMTP_SERVER=${SMTP_SERVER:-0.0.0.0}
+MAIL_FROM=${MAIL_FROM:-cfengine@example.com}
+MAIL_TO=${MAIL_TO:-admin@example.com}
+CFKEY=${CFENGINE_KEY:-/var/cfengine/ppkeys/localhost.pub}
 
 echo "Installing CFEngine Core 3.11.0-build1"
-
 pkg install cfengine
+cf-key --output-file=${CFENGINE_KEY}
 
-echo "Create key"
-cf-key
-
-CFKEY=/var/cfengine/ppkeys/localhost.pub
-
-ln -s /usr/local/bin/cf-* /var/cfengine/bin/
-
+echo "linking pkg binaries to work dir"
+ln -s /usr/local/bin/cf-* ${CFENGINE_WORK_DIR}
 
 echo "checkout repo"
-svnlite checkout ${CFENGINE_REPO_URL} ${CFENGINE_HOME}
+svnlite checkout ${CFENGINE_REPO_URL} ${CFENGINE_CHECKOUT_DIR}
 
-cd ${CFENGINE_WORK_DIR}
+echo "linking repo dirs to work directory"
+ln -s ${CFENGINE_CHECKOUT_DIR}/inputs ${CFENGINE_WORK_DIR}/
+ln -s ${CFENGINE_CHECKOUT_DIR}/masterfiles ${CFENGINE_WORK_DIR}/
+ln -s ${CFENGINE_CHECKOUT_DIR}/ ${CFENGINE_WORK_DIR}/
 
-CFENGINE_SVN_INPUTS=${CFENGINE_CHECKOUT_DIR}/trunk/cfengine/inputs
+echo "Setting up config files"
+
+CFENGINE_SVN_INPUTS=${CFENGINE_CHECKOUT_DIR}/inputs
 for file in ${CFENGINE_SVN_INPUTS}
 do
 	sed -i '' 's/{{POLICY_SERVER}}/${POLICY_SERVER}/' ${file}
@@ -39,7 +40,5 @@ do
 	sed -i '' 's/{{MAIL_TO}}/${MAIL_TO}/' ${file}
 done
 
-ln -s ${CFENGINE_CHECKOUT_DIR}/trunk/cfengine/inputs ${CFENGINE_WORK_DIR}/
-ln -s ${CFENGINE_CHECKOUT_DIR}/trunk/cfengine/masterfiles ${CFENGINE_WORK_DIR}/
-ln -s ${CFENGINE_CHECKOUT_DIR}/trunk/cfengine/masterfiles ${CFENGINE_WORK_DIR}/
-mkdir /var/cfengine/masterfiles
+cd ${CFENGINE_WORK_DIR}
+
